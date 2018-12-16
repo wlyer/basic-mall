4dd79725d92fc4d0f2d894aa2bd099e2ccf4eee3
wlyer <ziji931213>
2018-12-16 23:04:38 +0800

    init

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -7,7 +7,18 @@
   <section class="login dColumn">
     <img class="logo" src="../assets/images/logo.png"/>
     <div class="form-box">
-      <van-cell-group class="form">
+      <div class="login-form bb">
+        <i class="iconfont login-icon">&#xe633;</i><input type="text" class="flex1 login-input" placeholder="请输入账号"/>
+      </div>
+      <div class="login-form bb">
+        <i class="iconfont login-icon">&#xe7ba;</i><input type="password" class="flex1 login-input" placeholder="请输入密码"/>
+      </div>
+      <div class="login-form clearfix">
+        <span class="links fl" @click="toRegist">立即注册</span>
+        <span class="links rl" @click="toForget">忘记密码</span>
+      </div>
+      <van-button class="login-btn operation-btn" type="danger" @click="doLogin">登录</van-button>
+      <!--<van-cell-group class="form">
         <van-field
           v-model="loginForm.userCode"
           placeholder="请输入账号"
@@ -24,7 +35,7 @@
           <i class="iconfont" slot="label">&#xe7ba;</i>
         </van-field>
       </van-cell-group>
-      <van-button class="operation-btn" type="danger" @click="doLogin">登录</van-button>
+      <van-button class="operation-btn" type="danger" @click="doLogin">登录</van-button>-->
     </div>
   </section>
 </template>
@@ -72,7 +83,7 @@
 <style scoped lang="scss">
 .login{
   height: 100%;
-  background: url('../assets/images/login_bg.png') center no-repeat;
+ /* background: url('../assets/images/login_bg.png') center no-repeat;*/
   background-size: 100% 100%;
   display: flex;
   justify-content: center;
@@ -83,10 +94,31 @@
     margin-bottom: 30px;
   }
   .form-box{
-    text-align: center;
     width: 80%;
-    .form{
-      margin-bottom: 30px;
+    .login-form{
+      height: 45px;
+      line-height: 45px;
+      margin-bottom: 20px;
+      .login-icon {
+        width: 30px;
+        height: 30px;
+        display: inline-block;
+        line-height: 30px;
+      }
+      .login-input {
+        padding: 0 10px;
+        border: none;
+        background: none;
+        height: 30px;
+        line-height: 30px;
+        width: calc(100% - 30px);
+      }
+    }
+    .login-btn{
+      margin-top: 20px;
+      border-radius: 25px;
+      background-color: $my-theme-color;
+      border-color: $my-theme-color;
     }
   }
 }
